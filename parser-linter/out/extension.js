93d07126376f580dbcfcfc14df38bf1c81aa850c
Jannat Watts <jannat@sumologic.com>
2019-09-18 15:00:50 +0530

    correcting update diagnostic function

--- parser-linter/out/extension.js
+++ parser-linter/out/extension.js
@@ -23,6 +23,53 @@ function activate(context) {
     });
 }
 exports.activate = activate;
+function updateDiagnostics(document, collection) {
+    if (document) {
+        let lines = document.getText().split('\n');
+        let diag = [];
+        let reg_match_regex = new RegExp(/(r\|.+?(\n|$))|REGEX= *(.+?($|\n))/, 'ig');
+        for (let index = 0; index < lines.length; index++) {
+            let line = lines[index];
+            var match = reg_match_regex.exec(line);
+            if (!match) {
+            }
+            while (match) {
+                let match_exp = '';
+                if (match[3]) {
+                    match_exp = match[3];
+                }
+                else {
+                    match_exp = match[0].substring(2);
+                }
+                console.log(match);
+                try {
+                    let r = new RegExp(match_exp);
+                }
+                catch (exception) {
+                    var message = exception.message;
+                    diag.push({
+                        code: '',
+                        message: 'Not a Valid Regex',
+                        range: new vscode.Range(new vscode.Position(index, match.index), new vscode.Position(index, match.index + match_exp.length)),
+                        severity: vscode.DiagnosticSeverity.Error,
+                        source: '',
+                        relatedInformation: [
+                            new vscode.DiagnosticRelatedInformation(new vscode.Location(document.uri, new vscode.Range(new vscode.Position(index + 1, match.index), new vscode.Position(index + 1, match.index + match[0].length))), message)
+                        ]
+                    });
+                }
+                match = reg_match_regex.exec(line);
+            }
+            collection.set(document.uri, diag);
+            for (let index = 0; index < lines.length; index++) {
+                const element = lines[index];
+            }
+        }
+    }
+    else {
+        collection.clear();
+    }
+}
 function returnHoverInfo(word) {
     if (word == "ALIAS") {
         return new vscode.Hover({ language: "ALIAS",
@@ -464,48 +511,6 @@ This Attributes is  Specific to JSON Format" });
             value: "[<sourcetype>:<sourcetype name>]\n\
             sourcetype - which defines the entry point for the overall parser and contains attributes that control the overall execution of the parser"
         });
-    }
-}
-function updateDiagnostics(document, collection) {
-    if (document) {
-        let lines = document.getText().split('\n');
-        let diag = [];
-        for (let index = 0; index < lines.length; index++) {
-            let reg_match_regex = new RegExp(/r\|.+?( |\n|$)/, 'ig');
-            //let matches=document.getText().match(reg_match_regex);
-            let line = lines[index];
-            var match = reg_match_regex.exec(line);
-            // matches.forEach(element => {
-            //     console.log();
-            // });
-            while (match) {
-                console.log(match);
-                try {
-                    let r = new RegExp(match[0].substring(2));
-                }
-                catch (exception) {
-                    var message = exception.message;
-                    diag.push({
-                        code: '',
-                        message: 'Not a Valid Regex',
-                        range: new vscode.Range(new vscode.Position(index, match.index), new vscode.Position(index, match.index + match[0].length)),
-                        severity: vscode.DiagnosticSeverity.Error,
-                        source: '',
-                        relatedInformation: [
-                            new vscode.DiagnosticRelatedInformation(new vscode.Location(document.uri, new vscode.Range(new vscode.Position(index + 1, match.index), new vscode.Position(index + 1, match.index + match[0].length))), message)
-                        ]
-                    });
-                }
-                match = reg_match_regex.exec(line);
-            }
-            collection.set(document.uri, diag);
-            for (let index = 0; index < lines.length; index++) {
-                const element = lines[index];
-            }
-        }
-    }
-    else {
-        collection.clear();
     }
 }
 // this method is called when your extension is deactivated
--- parser-linter/src/extension.ts
+++ parser-linter/src/extension.ts
@@ -28,6 +28,57 @@ export function activate(context: vscode.ExtensionContext) {
     
 }
 
+function updateDiagnostics(document: vscode.TextDocument, collection: vscode.DiagnosticCollection): void {
+	if (document) {
+		let lines = document.getText().split('\n');
+		let diag=[];
+		let reg_match_regex = new RegExp(/(r\|.+?(\n|$))|REGEX= *(.+?($|\n))/, 'ig');
+		for (let index = 0; index < lines.length; index++) {
+
+			let line=lines[index];
+			var match = reg_match_regex.exec(line);
+			if(!match){
+
+			}
+			while (match) {
+				let match_exp='';
+				if(match[3]){
+					match_exp=match[3];
+				}
+				else{
+					match_exp=match[0].substring(2);
+				}
+				console.log(match);
+
+				try {
+					let r=new RegExp(match_exp);
+				}
+				catch (exception) {
+					var message = exception.message;
+					diag.push({
+						code: '',
+						message: 'Not a Valid Regex',
+						range: new vscode.Range(new vscode.Position(index, match.index), new vscode.Position(index, match.index+match_exp.length)),
+						severity: vscode.DiagnosticSeverity.Error,
+						source: '',
+						relatedInformation: [
+							new vscode.DiagnosticRelatedInformation(new vscode.Location(document.uri, new vscode.Range(new vscode.Position(index+1, match.index), new vscode.Position(index+1, match.index+match[0].length))), message)
+						]
+					});
+
+				}
+				match = reg_match_regex.exec(line);
+			}
+			collection.set(document.uri, diag);
+			for (let index = 0; index < lines.length; index++) {
+				const element = lines[index];
+			}
+		}
+	} else {
+		collection.clear();
+	}
+}
+
 function returnHoverInfo(word:string){
     if (word == "ALIAS") {
         return new vscode.Hover({language:"ALIAS",
@@ -505,52 +556,6 @@ This Attributes is  Specific to JSON Format"})
 
         })}
 }
-
-
-function updateDiagnostics(document: vscode.TextDocument, collection: vscode.DiagnosticCollection): void {
-    if (document) {
-        let lines = document.getText().split('\n');
-        let diag=[];
-        for (let index = 0; index < lines.length; index++) {
-            let reg_match_regex = new RegExp(/r\|.+?( |\n|$)/, 'ig');
-            //let matches=document.getText().match(reg_match_regex);
-            let line=lines[index];
-            var match = reg_match_regex.exec(line);
-            // matches.forEach(element => {
-            //     console.log();
-            // });
-            while (match) {
-                console.log(match);
-                
-                try {
-                    let r=new RegExp(match[0].substring(2));
-                }
-                catch (exception) {
-                    var message = exception.message;
-                    diag.push({
-                        code: '',
-                        message: 'Not a Valid Regex',
-                        range: new vscode.Range(new vscode.Position(index, match.index), new vscode.Position(index, match.index+match[0].length)),
-                        severity: vscode.DiagnosticSeverity.Error,
-                        source: '',
-                        relatedInformation: [
-                            new vscode.DiagnosticRelatedInformation(new vscode.Location(document.uri, new vscode.Range(new vscode.Position(index+1, match.index), new vscode.Position(index+1, match.index+match[0].length))), message)
-                        ]
-                    });
-                    
-                }
-                match = reg_match_regex.exec(line);
-            }
-            collection.set(document.uri, diag);
-            for (let index = 0; index < lines.length; index++) {
-                const element = lines[index];
-            }
-        }
-    } else {
-        collection.clear();
-    }
-}
-
 // this method is called when your extension is deactivated
 export function deactivate() {
 }
